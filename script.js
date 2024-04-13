354197b761b122b2e804ca3642748cb9a2a67e9d
Eder-TS <steder20@yahoo.com.br>
2024-04-13 19:56:50 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function EmailValidation(){
+    let email = document.getElementById("email").value
+    const emailPattern = /^[a-z0-9]+@+[a-z0-9]+[.a-z]+$/
+
+    let itsValid = email.match(emailPattern)
+
+    if (!itsValid){
+        console.log("erro")
+        console.log(email)
+        console.log(itsValid)
+    } else {
+        console.log(itsValid)
+    }
+}
